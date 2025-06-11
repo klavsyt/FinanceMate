881af072518fc83897d326e08276a4383fd98861
klavsyt <klavsyt_y@mail.ru>
2025-06-11 23:33:36 +0300

    .

--- frontend/__tests__/transactions.test.js
+++
@@ -1,9 +0,0 @@
-// Пример простого unit-теста для frontend
-// Замените someFunction на реальную функцию из transactions.js для проверки
-import { showTransactionModal } from "../transactions.js";
-
-describe("showTransactionModal", () => {
-  it("должен быть определён", () => {
-    expect(showTransactionModal).toBeDefined();
-  });
-});
